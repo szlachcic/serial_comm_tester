120a047d419438ebed35c6c72216152cd87175a3
Piotr Szlachcic <szlachcici49er@gmail.com>
2019-09-05 13:41:00 +0200

    motion tests done, not tested

--- test.py
+++ test.py
@@ -31,26 +31,49 @@ class USB_serial(serial.Serial):
 
     def loopRecive(self):
         while 1==1:
-            lock.acquire()
+            # lock.acquire(blocking=True, timeout=1)
             print(threading.currentThread())
-            print (self.usb.inWaiting())
             if self.usb.in_waiting != 0:
+                lock.acquire()
                 data = self.usb.readline()
+                lock.release()
                 first_char =  data[:1].decode('utf-8')
-
-                if first_char=='g' or first_char=='T' or first_char=='F':
+                print("check")
+                if first_char=='W' or first_char=='T' or first_char=='F':
                     self.buf_s.append(data)
                 elif first_char=='L' or first_char=='I' or first_char=='D':
-                    self.buf_s.append(data)
+                    self.buf_o.append(data)
                 elif first_char=='H' or first_char=='A' or first_char=='J' or first_char=='S' or first_char=='Y':
-                    self.buf_s.append(data)
+                    self.buf_m.append(data)
                 else: 
                     print('Incorrect comand: ')
                     print(data.decode('utf-8'))
+            
+            time.sleep(0.1)
+            
+class fileHandler:
+    # failed_tests = open('failed.txt', 'w')
+    # passed_tests = open('passed.txt', 'w')
+    
+    def __init__(self):
+        global failed_tests
+        global passed_tests
 
-            lock.release()
-           
-                # motion_test()
+    def __del__(self):
+        self.failed_tests.close()
+        self.passed_tests.close() 
+
+    def write_f(self, text):
+        print(text)
+        with open('failed.txt', 'a')  as f:
+            f.write(text)
+
+    def write_p(self, text):
+        print(text)
+        with open('passed.txt', 'a')  as f:
+            f.write(text)
+
+
 
 def random_test(group, cmd, y):
     x = random.randint(1,y)
@@ -62,33 +85,54 @@ def random_test(group, cmd, y):
 
 def motion_test():
     while 1==1:
-        lock.acquire()
+        lock.acquire(blocking=True, timeout=10)
         print(threading.currentThread())
-        print("lalala")
         cmd_list = ["H","A","J","S","Y"]
         cmd = random.choice(cmd_list)
-        cmd = "S"
-        data = random_test('test_drive', cmd, 3)
+        data = random_test('test_drive', cmd, 1)
         test_list = data["CMDS"]
-        
+        is_passed=0
+
+
         for x in test_list:
+            print("Kkkkkk")
             if x["DIR"] == 'T':
                 cmd_to_send = x["CMD_TO_SEND"].encode('utf-8')
                 print(cmd_to_send)
                 USB.send(cmd_to_send)
+                is_passed=1
             elif x["DIR"] == 'R':
                 t = time.time()
+                while 1==1:
 
-                while time.time() > t+x["TIMEOUT"]:
+                    if time.time() < t+(x["TIMEOUT"]/1000): 
+                        is_passed=0
+                        break
+
+                    time.sleep(0.1)
+
                     if len(USB.buf_m) != 0:
                         answ = USB.buf_m.pop()
-
                         if answ[1:] ==  x["CMD"] and answ[:2] == x["VAL"]:
+                            is_passed = 1
                             print("OKEY")
-                    else: print("Timeout")
-        lock.release()
+                            break
+                        else: 
+                            is_passed=0
+                            break
 
-        time.sleep(1)
+            if is_passed==0: break
+
+
+        if is_passed==1:
+            FILE.write_p("{}==> PASSED\n".format(data['DESC']))
+        else:
+            FILE.write_f("{}==> FAILED\n".format(data['DESC']))
+
+
+        lock.release()
+        time.sleep(0.1)
+
 
 def state_test():
     pass
@@ -100,14 +144,19 @@ if __name__=='__main__':
 
 
     USB = USB_serial()
-    th1 = threading.Thread(target = USB.loopRecive) 
+    FILE = fileHandler()
+
+    th1 = threading.Thread(target = USB.loopRecive)
+    # th1.daemon = True
     th2 = threading.Thread(target = motion_test) 
+    # th2.daemon = True
 
     th1.start()
     th2.start()
 
     th1.join()
     th2.join()
+    
     
     
   
